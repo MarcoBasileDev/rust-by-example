7966948471c6fb0a2af8193d50b47789717d4102
ItsStillBad <marco.basile1996@gmail.com>
2025-11-27 14:11:30 +0000

    add Display exercise

--- 1_HelloWorld/Display/src/main.rs
+++ 1_HelloWorld/Display/src/main.rs
@@ -1,3 +1,29 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let sign = if self.imag < 0.0 { '-' } else { '+' };
+        write!(f, "{} {}{}i", self.real, sign, self.imag.abs())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let c1 = Complex { real: 3.3, imag: 7.2 };
+    let c2 = Complex { real: 4.7, imag: -2.3 };
+
+    println!();
+    println!("Display: {}", c1);
+    println!("Debug: {:?}", c1);
+
+    println!();
+    println!("Display: {}", c2);
+    println!("Debug: {:?}", c2);
+
 }
