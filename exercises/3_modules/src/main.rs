b5f041db2d2a4d4a25b199f2b36bbe497c044bcd
ItsStillBad <marco.basile1996@gmail.com>
2025-11-29 20:26:42 +0100

    use lib

--- exercises/3_modules/src/main.rs
+++ exercises/3_modules/src/main.rs
@@ -1,19 +1,5 @@
-// 1. Organize code into a library without changing the output of the program.
-//
-// For each step of this exercise, you should be able to run the program before and after your
-// change without affecting the output of the program.
-//
-// Move the constants below (FIRST, SECOND, and THIRD) into the library:
-// - Create a `src/lib.rs` file
-// - Move all of the constants into lib.rs
-// - Make the constants public by adding the `pub` keyword in front of them
-// - Add `use` statement(s) to main.rs to bring the constants into scope.
-//
-// Hint: the name of the library is defined in Cargo.toml
+use modules::{FIRST, SECOND, THIRD};
 
-const FIRST: i32 = 1;
-const SECOND: i32 = 2;
-const THIRD: i32 = 3;
 
 // 2. Create a library module named `sound` and move the animal functions into it.
 //
