0879cded39efd086836bf19ef522c130d8ff8bda
ItsStillBad <marco.basile1996@gmail.com>
2025-11-30 16:07:10 +0100

    add challenges variants

--- exercises/e_control_flow/src/main.rs
+++ exercises/e_control_flow/src/main.rs
@@ -16,6 +16,20 @@ fn main() {
         count
     );
 
+    count = 0;
+    bunnies = 2;
+
+    // while variants
+    while bunnies < 500 {
+        count += 1;
+        bunnies *= 2;
+    }
+
+    println!(
+        "Bunnies doubled {} times before there were more than 500 - while variant",
+        count
+    );
+
     let mut sum = 0;
 
     for num in 7..=23 {
@@ -23,6 +37,22 @@ fn main() {
     }
 
     println!("The sum is {}", sum);
+
+    // loop variant
+    sum = 0;
+    let mut num = 7;
+
+    loop {
+        sum += num;
+
+        if num == 23 {
+            break;
+        }
+
+        num += 1;
+    }
+
+    println!("The sum is {} - loop variant", sum);
 
     let mut fives: Vec<i32> = vec![];
     let mut number = 5;
@@ -33,6 +63,15 @@ fn main() {
     }
 
     println!("Here are the first 12 multiples of 5: {:?}", fives);
+
+    let mut fives: Vec<i32> = vec![];
+    number = 5;
+
+    for i in  1..=12{
+        fives.push(number * i);
+    }
+
+    println!("Here are the first 12 multiples of 5: {:?} - for variant", fives);
 
     let mut total = 0;
     let numbers = vec![0, 1, 2, 3, 4, 5];
@@ -48,10 +87,4 @@ fn main() {
     }
 
     println!("The total is {}", total);
-
-    // Challenge: Change the implementation of your answers to #1-#3 as follows:
-    //
-    // - Change #1 to use `while`
-    // - Change #2 to use `loop`
-    // - Change #3 to use `for` and a range (multiply the range value by 5 inside your loop before
 }
