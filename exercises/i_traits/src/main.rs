90fa31b1127350c10beb5335607a26b1faee8ffa
ItsStillBad <marco.basile1996@gmail.com>
2025-11-30 19:56:06 +0100

    add traits exercises

--- exercises/i_traits/src/main.rs
+++ exercises/i_traits/src/main.rs
@@ -1,94 +1,89 @@
 // Silence some warnings so they don't distract from the exercise.
-#![allow(dead_code, unused_mut)]
+#![allow(dead_code)]
 
-// 1. Create a struct named `Polygon` with the fields and their types listed below. Then build the
-// program with `cargo build` to ensure you don't have any syntax errors.
+// 1a: Create a `Colorful` trait with a single `color` method which takes an immutable reference to
+// self and returns a String describing that item's color.
 //
-// - name - String
-// - sides - u32
-// - visible - bool
+// You must also complete 1b before the code will compile.
 
-// struct Polygon ...
+// trait Colorful ...
 
-// 2. Create an implementation block for the `Polygon` struct.
+// 1b. Implement the `Colorful` trait for the `Hat` struct:
 //
-// In the implementation block define an associated function named `new` that:
-// - accepts an argument `name` of type `String`
-// - returns a `Polygon` (you may use `Self` as an alias for `Polygon` inside of the `impl` block)
-//   - with `name` set to the value from the `name` argument.
-//   - with `sides` set to `3`
-//   - with `visible` set to `true`
+// The `colorful` method of the `Colorful` trait implementation for the `Hat` struct should return
+// the following String values:
+// - "red" if the size is 0 through 5
+// - "green" - if the size is 6 or 7
+// - "blue" - for any other size
 //
-// NOTE: Associated functions do NOT take a form a `self` as their first argument (that would turn
-// the function into a method)
-//
-// Then build the program with `cargo build` to ensure you don't have any syntax errors.
+// The code should compile once 1b is complete, but there will be no output until 1c is completed.
 
-// impl Polygon ...
+struct Hat {
+    size: i32,
+}
+
+// impl Colorful for Hat ...
 
 fn main() {
-    // 3. Create a new, mutable polygon variable by calling the Polygon's `new` associated function.
-    //
-    // - Use the scope operator `::` to access an associated function of a struct.
-    // - Use the name "George". Remember to convert the string literal to a String!
-    //
-    // Then uncomment and run the code below to see a message about the polygon.
+    // 1c. Uncomment and run the code below. If you correctly implemented Colorful for Hat, then
+    // the order of the colors in the output will be red, green, and blue.
 
-    // let mut polygon = ...
-    // println!(
-    //     "I see a {}-sided polygon named {}!",
-    //     polygon.sides, polygon.name
-    // );
+    // let small_hat = Hat { size: 2 };
+    // let medium_hat = Hat { size: 7 };
+    // let large_hat = Hat { size: 100 };
+    // describe_three_hats(&small_hat, &medium_hat, &large_hat);
 
-    // 4. In the `impl Polygon` block above:
-    //
-    // - Add a method named `shape` which
-    //   - takes an immutable reference to self
-    //   - returns a String
-    //   - depending on the value of the `sides` field returns the following strings:
-    //     - "triangle" - for 3 sides
-    //     - "square" - for 4 sides
-    //     - "pentagon" - for 5 sides
-    //     - "polygon" - for any other number of sides
+    // 2. Implement the Colorful trait for the type i32. The `colorful` method for an i32 should
+    // return these String values:
+    //   - "orange" - If the number is even (see hint)
+    //   - "purple" - If the number is odd
     //
     // Then uncomment and run the code below.
-
-    // println!(
-    //     "The polygon named {} is a {}",
-    //     polygon.name,
-    //     polygon.shape()
-    // );
-
-    // 5. In the `impl Polygon` block above:
     //
-    // - Add a method named `increment_sides` that
-    //   - takes a mutable reference to self
-    //   - returns nothing
-    //   - adds 1 to the `sides` field
-    //
-    // Then uncomment and run the code below.
+    // Hint: You may want to use the `is_even` function (see the bottom of this file).
 
-    // for _ in 0..3 {
-    //     polygon.increment_sides();
-    //     println!(
-    //         "The polygon now has {} sides and is the shape of a {}",
-    //         polygon.sides,
-    //         polygon.shape()
-    //     );
-    // }
+    // println!("4 is {}", 4.color());
+    // println!("5 is {}", 5.color());
 
-    // Challenge: Move the `Polygon` struct and impl blocks to lib.rs and put `pub` in front of the
-    // fields, methods, and associated function that need to be public. Then add `use` statements to
-    // this file so that the program will run and produce the same output as before.
+    // 3. Let's replace the is_even function with a trait implementation!
+    //
+    // - Comment out the is_even function at the bottom of this file so you can't use it anymore.
+    // - Create a trait named `EvenOdd` with a method `is_even`. It should take an immutable
+    //   reference to self and return a bool.
+    // - Implement EvenOdd for i32. You can copy-and-paste the logic from the is_even function that
+    //   you commented out earlier.
+    // - Refactor the `colorful` method for `i32` to use the is_even method.
+    //
+    // Then you should be able to run the code without any changes to the output.
 
-    // Challenge 2: Make the Polygon's `sides` field private by removing the `pub`. Add a method
-    // that you can call to get the value of the `sides` field without directly accessing it.
-    // "Getter" methods for `Copy`† types are typically named the same as their private field, e.g.
-    // `pub fn sides(...)` would be the method to return the value of the private `sides` field.
+    // Challenge: Write a generic function named `fortune` that takes anything that implements the
+    // Colorful trait and prints out the color in some message (for example: "The color I see in
+    // your future is ..."). Then uncomment and run the code below.
     //
-    // Modify the code in this file to use the new method instead of accessing the `sides` field
-    // directly. You should be able to run the program and still get the same output.
-    //
-    // †Copy types (types that implement the Copy trait) are briefly described in the Traits lesson,
-    // and more thoroughly explained in Ultimate Rust 2.
+    // Hint: There's a bit of commented-out code below the main function to help you get started.
+
+    // fortune(small_hat);
+    // fortune(2);
+}
+
+// fn fortune<T: Colorful>(...
+
+// A function used by some provided code.
+fn describe_three_hats(hat1: &Hat, hat2: &Hat, hat3: &Hat) {
+    for hat in [hat1, hat2, hat3] {
+        let largeness = if hat.size < 3 {
+            "small"
+        } else if hat.size < 9 {
+            "medium"
+        } else {
+            "large"
+        };
+        println!("The {} hat is {}", largeness, hat.color());
+    }
+}
+
+// You can use this function to check if a number is even (true) or odd (false).
+// You should comment out this function for #3.
+fn is_even(number: i32) -> bool {
+    number % 2 == 0
 }
