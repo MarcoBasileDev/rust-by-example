8c73ab6e4e1277e7d8cfa476f22e5ac0bae554c2
ItsStillBad <marco.basile1996@gmail.com>
2025-12-02 10:51:06 +0100

    use cargo fmt

--- exercises_2/a_idiomatic/src/main.rs
+++ exercises_2/a_idiomatic/src/main.rs
@@ -8,8 +8,19 @@
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
-const pi:f32=3.14159265358979323846;
-fn count_to_5()->i32{let mut foo =0;loop{if foo>pi as i32{if foo > 5{break;}}foo=foo+1;}return 5;}
+const pi: f32 = 3.14159265358979323846;
+fn count_to_5() -> i32 {
+    let mut foo = 0;
+    loop {
+        if foo > pi as i32 {
+            if foo > 5 {
+                break;
+            }
+        }
+        foo = foo + 1;
+    }
+    return 5;
+}
 fn main() {
     println!("I can count to {}", count_to_5());
 }
