2f3c488b758d17a805f08e43818cc3d792c8f1f2
MarcoBasileDev <marco.basile1996@gmail.com>
2025-12-09 17:48:46 +0000

    add game state

--- exercises_2/project/src/main.rs
+++ exercises_2/project/src/main.rs
@@ -2,13 +2,20 @@ use rusty_engine::prelude::*;
 
 #[derive(Resource)]
 struct GameState {
-    health_left: i32,
+    current_score: u32,
+    high_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
 }
 
 fn main() {
     let mut game = Game::new();
 
-    // setup game here
-
-    game.run(GameState { health_left: 42 });
+    let game_state = GameState {
+        current_score: 0,
+        high_score: 2345,
+        enemy_labels: Vec::new(),
+        spawn_timer: Timer::from_seconds(10.0, TimerMode::Once),
+    };
+    game.run(game_state);
 }
