6d159ccd96091770ec32ee3b25cf24625671598f
MarcoBasileDev <marco.basile1996@gmail.com>
2025-12-09 17:42:48 +0000

    engine initialization

--- exercises_2/project/src/main.rs
+++ exercises_2/project/src/main.rs
@@ -1,12 +1,14 @@
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
-struct Empty;
+struct GameState {
+    health_left: i32,
+}
 
 fn main() {
     let mut game = Game::new();
 
     // setup game here
 
-    game.run(Empty);
+    game.run(GameState { health_left: 42 });
 }
