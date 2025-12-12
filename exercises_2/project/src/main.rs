dac9240699c26efe4c50635d19f6b874d54277a3
ItsStillBad <marco.basile1996@gmail.com>
2025-12-12 08:47:16 +0100

    add collisions

--- exercises_2/project/src/main.rs
+++ exercises_2/project/src/main.rs
@@ -26,12 +26,22 @@ fn main() {
     player.translation = Vec2::new(0.0, 0.0);
     player.rotation = SOUTH_WEST;
     player.scale = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    game_state.current_score += 1;
-    println!("Current score: {}", game_state.current_score);
+    // game_state.current_score += 1;
+    // println!("Current score: {}", game_state.current_score);
+    for event in engine.collision_events.drain(..) {
+        println!("{:?}", event);
+    }
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
 }
