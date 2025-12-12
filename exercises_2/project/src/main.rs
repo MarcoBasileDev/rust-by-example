61c1da4bc22901b0804a5f0ea81a0d0c86d53164
ItsStillBad <marco.basile1996@gmail.com>
2025-12-12 09:26:37 +0100

    add score counter

--- exercises_2/project/src/main.rs
+++ exercises_2/project/src/main.rs
@@ -2,7 +2,7 @@ use rusty_engine::prelude::*;
 
 #[derive(Resource)]
 struct GameState {
-    current_score: u32,
+    score: u32,
     high_score: u32,
     ferris_index: i32,
     spawn_timer: Timer,
@@ -11,8 +11,8 @@ struct GameState {
 impl Default for GameState {
     fn default() -> Self {
         Self {
+            score: 0,
             high_score: 0,
-            current_score: 0,
             ferris_index: 0,
             spawn_timer: Timer::default(),
         }
@@ -27,6 +27,12 @@ fn main() {
     player.rotation = SOUTH_WEST;
     player.scale = 1.0;
     player.collision = true;
+
+    let score = game.add_text("score", "Score: 0");
+    score.translation = Vec2::new(520.0, 320.0);
+
+    let high_score = game.add_text("high_score", "High score: 0");
+    high_score.translation = Vec2::new(-520.0, 320.0);
 
     game.add_logic(game_logic);
     game.run(GameState::default());
@@ -43,8 +49,16 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 }
             }
 
-            game_state.current_score += 1;
-            println!("Current score: {}", game_state.current_score);
+            game_state.score += 1;
+            let score = engine.texts.get_mut("score").unwrap();
+            score.value = format!("Score: {}", game_state.score);
+
+            if game_state.score >= game_state.high_score {
+                game_state.high_score = game_state.score;
+
+                let high_score = engine.texts.get_mut("high_score").unwrap();
+                high_score.value = format!("High score: {}", game_state.high_score);
+            }
         }
     }
 
@@ -76,5 +90,12 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
             feris.translation = mouse_location;
             feris.collision = true;
         }
+    }
+
+    // Reset score
+    if engine.keyboard_state.just_pressed(KeyCode::R) {
+        game_state.score = 0;
+        let score = engine.texts.get_mut("score").unwrap();
+        score.value = "Score: 0".to_string();
     }
 }
