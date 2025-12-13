645c5305bdab926701cccffa17b1e3f31f4303d0
ItsStillBad <marco.basile1996@gmail.com>
2025-12-13 16:57:40 +0100

    add music

--- exercises_2/road_race/src/main.rs
+++ exercises_2/road_race/src/main.rs
@@ -18,6 +18,15 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    // Create the player sprite
+    let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
+    player1.translation.x = -500.0;
+    player1.layer = 10.0;
+    player1.collision = true;
+
+    // Start some background music
+    game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.1);
+
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
