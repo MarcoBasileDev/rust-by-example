f52fa22f441873a2591f59631a6b9b7a5b5d1f3c
ItsStillBad <marco.basile1996@gmail.com>
2025-12-13 16:44:18 +0100

    setup game

--- exercises_2/road_race/src/main.rs
+++ exercises_2/road_race/src/main.rs
@@ -1,5 +1,20 @@
 use rusty_engine::prelude::*;
 
+#[derive(Resource)]
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            health_amount: 5,
+            lost: false,
+        }
+    }
+}
+
 fn main() {
     let mut game = Game::new();
 
