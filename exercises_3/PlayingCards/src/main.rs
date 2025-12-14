7a180f911c105d2c2111a73df9be76fef509eb67
ItsStillBad <marco.basile1996@gmail.com>
2025-12-14 12:25:36 +0100

    refactoring deck cards initialization

--- exercises_3/PlayingCards/src/main.rs
+++ exercises_3/PlayingCards/src/main.rs
@@ -3,18 +3,24 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
 
-    let mut deck = Deck { cards: Vec::new() };
-
-    for suit in &suits {
-        for value in &values {
-            deck.cards.push(format!("{} of {}",value, suit));
+        let mut cards = vec![];
+        for suit in &suits {
+            for value in &values {
+                cards.push(format!("{} of {}",value, suit));
+            }
         }
+        let deck = Deck { cards };
+        deck
     }
+}
 
+fn main() {
+    let deck = Deck::new();
 
-    println!("Here's your deck: {:?}", deck);
+    println!("Here's your deck: {:#?}", deck);
 }
