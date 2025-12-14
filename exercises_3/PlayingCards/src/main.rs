09dcada593d38ff8614ef7bfad9a06a2a16e726a
ItsStillBad <marco.basile1996@gmail.com>
2025-12-14 18:10:32 +0100

    add deal logic

--- exercises_3/PlayingCards/src/main.rs
+++ exercises_3/PlayingCards/src/main.rs
@@ -23,6 +23,10 @@ impl Deck {
         let mut rng = rand::rng();
         self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
@@ -31,4 +35,7 @@ fn main() {
 
     deck.shuffle();
     println!("Here's your shuffled deck: {:#?}", deck);
+
+    let cards = deck.deal(3);
+    println!("Here's your hand: {:#?}", cards);
 }
