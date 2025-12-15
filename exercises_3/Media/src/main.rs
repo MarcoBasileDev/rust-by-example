45f92b4794ab38a3ff6d3779dc38a39edee42415
ItsStillBad <marco.basile1996@gmail.com>
2025-12-15 17:31:31 +0100

    add enums

--- exercises_3/Media/src/main.rs
+++ exercises_3/Media/src/main.rs
@@ -5,5 +5,25 @@ enum Media {
     Audiobook { title: String },
 }
 
+fn print_media(media: &Media) {
+    println!("{:?}", media);
+}
+
 fn main() {
+    let audiobook = Media::Audiobook {
+        title: String::from("Media book"),
+    };
+    print_media(&audiobook);
+
+    let book = Media::Book {
+        title: String::from("Book"),
+        author: String::from("Book author"),
+    };
+    print_media(&book);
+
+    let movie = Media::Movie {
+        title: String::from("movie"),
+        director: String::from("Movie director"),
+    };
+    print_media(&movie);
 }
