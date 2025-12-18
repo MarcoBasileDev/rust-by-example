3387e38d7497b207300a394e0dbb36593803fb2b
MarcoBasileDev <marco.basile1996@gmail.com>
2025-12-18 15:49:37 +0100

    add add_string method

--- exercises_3/ShoppingCart/src/main.rs
+++ exercises_3/ShoppingCart/src/main.rs
@@ -1,3 +1,20 @@
+mod basket;
+mod stack;
+mod container;
+
+use basket::Basket;
+use stack::Stack;
+use crate::container::Container;
+
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut b1 = Basket::new(String::from("test"));
+
+    let mut s1 = Stack::new(vec![String::from("test")]);
+
+    add_string(&mut b1, String::from("test"));
+    add_string(&mut s1, String::from("test"));
 }
