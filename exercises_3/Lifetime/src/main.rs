11d067d7183b51e79afd777f795c53f7512f8748
MarcoBasileDev <marco.basile1996@gmail.com>
2025-12-18 12:47:35 +0100

    add longest_language fn

--- exercises_3/Lifetime/src/main.rs
+++ exercises_3/Lifetime/src/main.rs
@@ -18,6 +18,14 @@ fn last_language(languages: &[String]) -> &str {
     languages.last().unwrap()
 }
 
+fn longest_language<'a>(lang_a: &'a str, lang_b: &'a str) -> &'a str {
+    if lang_a.len() >= lang_b.len() {
+        lang_a
+    } else {
+        lang_b
+    }
+}
+
 fn main() {
     let languages = vec![
         String::from("rust"),
@@ -30,4 +38,7 @@ fn main() {
 
     let last = last_language(&languages);
     println!("{}", last);
+
+    let longest = longest_language("go", "Rust");
+    println!("{}", longest);
 }
