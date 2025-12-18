ecf6c3bee9ef35f675cb25599eca64bfbdf2d7c8
MarcoBasileDev <marco.basile1996@gmail.com>
2025-12-18 15:07:41 +0100

    add solve_second_version

--- exercises_3/Generics/src/main.rs
+++ exercises_3/Generics/src/main.rs
@@ -8,11 +8,22 @@ fn solve_first_version<T: Float>(a: T, b: T) -> f64 {
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
+// Second version - we can pass in any type of numbers
+fn solve_second_version<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
 
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
 
 fn main() {
-    let a: f32 = 3.0;
+    let a = 3.0;
     let b: f32 = 4.0;
 
     println!("{}", solve_first_version(a, b));
+
+    println!("{}", solve_second_version(3u8, 4.0f32));
+    println!("{}", solve_second_version(10i32, 2.5f64));
+    println!("{}", solve_second_version(7usize, 1u16));
+
 }
