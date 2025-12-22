91a679905c3f956a056af946c826f92480394260
ItsStillBad <marco.basile1996@gmail.com>
2025-12-22 14:36:10 +0100

    serve static content

--- axium_askama/todo/src/main.rs
+++ axium_askama/todo/src/main.rs
@@ -2,16 +2,21 @@ use askama::Template;
 use axum::response::{Html, IntoResponse};
 use axum::Router;
 use axum::routing::get;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    let server_dir = ServeDir::new("static");
+
     let app = Router::new()
         .route("/", get(home))
         .route("/create", get(create_todo))
         .route("/todos", get(todos))
         .route("/login", get(login_handler))
-        .route("/signup", get(signup_handler));
+        .route("/signup", get(signup_handler))
+        .nest_service("/static", server_dir);
 
     axum::serve(listener, app).await.unwrap();
 }
