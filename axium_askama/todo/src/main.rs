ad6dffe5c8feae8dd33a4c7fc78e7c1cfbf7a6d8
ItsStillBad <marco.basile1996@gmail.com>
2025-12-23 11:00:16 +0100

    add logger

--- axium_askama/todo/src/main.rs
+++ axium_askama/todo/src/main.rs
@@ -1,11 +1,17 @@
-use todo::routes;
+use todo::{init, routes};
 
 #[tokio::main]
 async fn main() {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
-        .unwrap();
+        .expect("Failed to bind address");
+
+    init::logging();
+
+    tracing::info!("Listening on: {}", listener.local_addr().unwrap());
 
     let app = routes::routes();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to run server");
 }
