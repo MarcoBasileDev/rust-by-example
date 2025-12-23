910e28fd4f846e40dfcd8916790376aada7632b2
ItsStillBad <marco.basile1996@gmail.com>
2025-12-23 11:32:35 +0100

    improve logging

--- axium_askama/todo/src/routes.rs
+++ axium_askama/todo/src/routes.rs
@@ -2,8 +2,15 @@ use crate::handlers::auth::{login_handler, signup_handler};
 use crate::handlers::public::home;
 use crate::handlers::todos::{create_todo, todos};
 use axum::Router;
+use axum::body::Body;
+use axum::http::Request;
+use axum::response::Response;
 use axum::routing::get;
+use std::time::Duration;
+use tower_http::classify::ServerErrorsFailureClass;
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
+use tracing::Span;
 
 pub fn routes() -> Router {
     let server_dir = ServeDir::new("static");
@@ -14,7 +21,34 @@ pub fn routes() -> Router {
         .route("/todos", get(todos))
         .route("/login", get(login_handler))
         .route("/signup", get(signup_handler))
-        .nest_service("/static", server_dir);
+        .nest_service("/static", server_dir)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|_: &Request<Body>| tracing::info_span!("http-request"))
+                .on_request(on_request)
+                .on_response(on_response)
+                .on_failure(on_failure),
+        );
 
     app
+}
+
+fn on_request(request: &Request<Body>, _: &Span) {
+    tracing::info!(
+        "Request stated: method {} path {}",
+        request.method(),
+        request.uri().path()
+    );
+}
+
+fn on_response(response: &Response<Body>, latency: Duration, _: &Span) {
+    tracing::info!(
+        "Response generated: status {} in {:?}",
+        response.status(),
+        latency
+    );
+}
+
+fn on_failure(error: ServerErrorsFailureClass, latency: Duration, _: &Span) {
+    tracing::error!("Request failed: {:?} after {:?}", error, latency);
 }
