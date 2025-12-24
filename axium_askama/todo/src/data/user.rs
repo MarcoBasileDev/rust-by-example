5d61f92d476edc5ec231503b7705c1618b629b23
ItsStillBad <marco.basile1996@gmail.com>
2025-12-24 11:31:18 +0100

    cargo fmt create_user fn

--- axium_askama/todo/src/data/user.rs
+++ axium_askama/todo/src/data/user.rs
@@ -1,6 +1,10 @@
 use sqlx::PgPool;
 
-pub async fn create_user(pool: &PgPool, email: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn create_user(
+    pool: &PgPool,
+    email: &str,
+    password: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let hashed_psw = bcrypt::hash(password, bcrypt::DEFAULT_COST)?;
     let bytea_hash = hashed_psw.as_bytes();
 
@@ -9,7 +13,13 @@ pub async fn create_user(pool: &PgPool, email: &str, password: &str) -> Result<(
     // sqlx::query(&query).bind(email).bind(bytea_hash).execute(pool).await?;
 
     // Or like this
-    sqlx::query!("INSERT INTO users (email, password_hash) VALUES ($1, $2)", email, bytea_hash).execute(pool).await?;
+    sqlx::query!(
+        "INSERT INTO users (email, password_hash) VALUES ($1, $2)",
+        email,
+        bytea_hash
+    )
+    .execute(pool)
+    .await?;
 
     Ok(())
 }
