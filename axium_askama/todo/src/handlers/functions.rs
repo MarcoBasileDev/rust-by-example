63dfa31a81e86c272164b20e547bc6acd5b713af
ItsStillBad <marco.basile1996@gmail.com>
2025-12-26 17:30:37 +0100

    remove unused fn

--- axium_askama/todo/src/handlers/functions.rs
+++ axium_askama/todo/src/handlers/functions.rs
@@ -7,7 +7,3 @@ pub fn previous_page(page: i32) -> i32 {
 pub fn next_page(page: i32) -> i32 {
     page + 1
 }
-
-pub fn todo_date(date: chrono::DateTime<Utc>) -> String {
-    date.format("%d %b %Y at %I:%M%P").to_string()
-}
